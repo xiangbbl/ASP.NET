2bf33068bde15d6ddee42c0992510ed1ffa44379
xiangbbl <55719651+xiangbbl@users.noreply.github.com>
2021-09-13 20:20:57 -0500

    Add project files.

---
+++ WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
